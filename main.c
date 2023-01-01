d03e86b763ab599edb6216c65e687929bc623792
Ruan <ruan10vr@gmail.com>
2023-01-01 17:48:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
     
-    
+    //Registrando nova pessoa
+    int idade;
+    int s = 1;
+    char sexo;
+
+    printf("Digite sua idade\n");
+    scanf("%d", idade);
+
+    //Confirma se o sexo é valido
+    while (s == 1)
+    {
+        printf("Digite seu sexo: m - masculino | f - feminino\n");
+        scanf("%s", sexo);
+
+        if (sexo == 'm' || sexo == 'f')
+        {
+            sexo = 0;
+        } 
+        else
+        {
+            printf("Sexo invalido, digite novamente\n");
+        }     
+    }
+
+   
     return 0;
 }
