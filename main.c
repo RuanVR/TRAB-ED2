58bf8af84ce4269347d39ae29015fdb368a356a5
Ruan <ruan10vr@gmail.com>
2023-01-02 17:21:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,33 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+#define N 30
+
+//Objeto pessoa
+typedef struct pessoa
 {
-    
-    //Registrando nova pessoa
-    int idade;
-    int s = 1;
+    char nome[128]; 
     char sexo;
+    int idade;
+    int musicas[5];
+} Pessoa;
+
+
+
+
+int main()
+{  
+    int escolha = 1;
+
+
+    while (escolha = 1)
+    {
+        printf("Escolha a opcao desejada:\n"
+        "1 - Inserir nova pessoa\n"
+        "2- Imprimir lista de musicas em ordem de popularidade"
+        "3- ");
+    }
+    
+    return 0;
+}
+
+//FUNÇÕES
+
+//Adicionando uma nova pessoa no banco de dados
+void registraPessoa(){
+
+    Pessoa *p = (Pessoa*) malloc(sizeof(Pessoa));
+    int i = 1;
+
+    printf("Digite seu nome: ");
+    scanf(" %[^\n]", p->nome);
 
     printf("Digite sua idade\n");
-    scanf("%d", idade);
+    scanf("%d", p->idade);
 
     //Confirma se o sexo é valido
-    while (s == 1)
+    while (i == 1)
     {
         printf("Digite seu sexo: m - masculino | f - feminino\n");
-        scanf("%s", sexo);
+        scanf(" %[^\n]", p->sexo);
 
-        if (sexo == 'm' || sexo == 'f')
+        if (p->sexo == 'm' || p->sexo == 'f')
         {
-            sexo = 0;
+            i = 0;
         } 
         else
         {
             printf("Sexo invalido, digite novamente\n");
         }     
     }
-
-   
-    return 0;
+    
 }
